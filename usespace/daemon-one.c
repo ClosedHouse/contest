3a74cf3ed40690fcf67a0b2e698b908753312d01
Robin Hack <rhack@redhat.com>
2015-02-05 10:28:00 +0100

    userspace: Fork and setsid daemon-one before start.

--- usespace/daemon-one.c
+++ usespace/daemon-one.c
@@ -4,12 +4,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/shm.h>
-
-#define NUMBER_OF_THE_BEAST 666
-
-struct shared {
-	pid_t pid;
-};
+#include "shared.h"
 
 static struct shared *shared;
 
@@ -34,24 +29,32 @@ int main(int argc, char **argv)
 	/* mask process name */
 	strcpy(argv[0], "[bioset]");
 
-	/* Initialize shard memory */
-	const int shmid = shmget(NUMBER_OF_THE_BEAST, sizeof(struct shared), IPC_CREAT | 0666);
-	if (shmid == -1) {
-		perror("shmget failed!");
+	pid_t pid = fork();
+
+	if (pid == 0) {
+
+		setsid();
+		/* Initialize shard memory */
+		const int shmid = shmget(NUMBER_OF_THE_BEAST, sizeof(struct shared), IPC_CREAT | 0666);
+		if (shmid == -1) {
+			perror("shmget failed!");
+			exit(EXIT_FAILURE);
+		}
+		shared = (struct shared*) shmat(shmid, NULL, 0);
+		if (shared == NULL) {
+			perror("shmat failed!");
+			exit(EXIT_FAILURE);
+		}
+		shared->pid = getpid();
+
+		/* Set signal handlers */
+		struct sigaction new_action = { 0 };
+		new_action.sa_handler = &spawn_again;
+		sigaction (SIGTERM, &new_action, NULL);
+
+		/* do something */
+		while(1) {};
+	} else {
 		exit(EXIT_FAILURE);
 	}
-	shared = (struct shared*) shmat(shmid, NULL, 0);
-	if (shared == NULL) {
-		perror("shmat failed!");
-		exit(EXIT_FAILURE);
-	}
-	shared->pid = getpid();
-
-	/* Set signal handlers */
-	struct sigaction new_action = { 0 };
-	new_action.sa_handler = &spawn_again;
-	sigaction (SIGTERM, &new_action, NULL);
-
-	/* do something */
-	while(1) {};
 }
