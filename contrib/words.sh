49f6272b0ceb0f764d1d7f4fbca32eea5beaccf9
Robin Hack <hack.robin@gmail.com>
2015-04-02 12:36:09 +0200

    contrib: Added some char generators.
    It's nice to play this on TV. It's part of atmosphere of contest.

---
+++ contrib/words.sh
@@ -0,0 +1,1 @@
+while :; do for p in Y o u " " h a v e " " b e e n " " c o m p r o m i s e d ! " " R e s i s t a n c e " " i s " " f u t i l e .; do echo -n "$p"; sleep 0.2; done; done
---
+++ contrib/zeroes.sh
@@ -0,0 +1,1 @@
+while :; do echo -n $(($RANDOM % 2)); sleep 0.5; done
